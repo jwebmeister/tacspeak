13a5f41de2d44ecbdab0bcff19841f22e0c30d57
JWebmeister <jwebmeister@gmail.com>
2023-12-08 22:18:07 +1100

    added select team action, keybind mappings, hold cmd

--- tacspeak/grammar/_readyornot.py
+++ tacspeak/grammar/_readyornot.py
@@ -9,6 +9,7 @@ from dragonfly import (BasicRule, CompoundRule, MappingRule, RuleRef, Repetition
                        Function, Choice, IntegerRef, Grammar, Alternative, Literal, Text,
                        AppContext)
 from dragonfly.engines.backend_kaldi.dictation import UserDictation as Dictation
+from dragonfly.actions import (Key, Mouse)
 
 import kaldi_active_grammar
 
@@ -26,13 +27,60 @@ grammar_priority = Grammar("ReadyOrNot_priority",
 # ---------------------------------------------------------------------------
 # Rules which will be added to our grammar
 
-colors = {
+# Will map keybindings to print()
+DEBUG_NOCMD_PRINT_ONLY = True
+
+# the minimum time between keys state changes (e.g. pressed then released), 
+# it's to make sure key presses are registered in-game
+min_delay = 3.3  # 100/(30 fps) = 3.3 (/100 seconds between frames)
+
+# map of action to in-game key bindings
+# https://dragonfly.readthedocs.io/en/latest/actions.html#key-names
+ingame_key_bindings_kb = {
+    "gold": "f5",
+    "blue": "f6",
+    "red": "f7",
+    "cmd_1": "1",
+    "cmd_2": "2",
+    "cmd_3": "3",
+    "cmd_4": "4",
+    "cmd_5": "5",
+    "cmd_6": "6",
+    "cmd_7": "7",
+    "cmd_8": "8",
+    "cmd_9": "9",
+    "cmd_back": "tab",
+    "hold_cmd": "shift",
+    "default_cmd": "z",
+}
+
+ingame_key_bindings_mouse = {
+    "cmd_menu": "middle",
+}
+
+def debug_print_key(device, key):
+    print(f'({device}_{key})')
+
+if DEBUG_NOCMD_PRINT_ONLY:
+    map_ingame_key_bindings = {k: Function(debug_print_key, device='kb', key=v) for k, v in ingame_key_bindings_kb.items()}
+    map_ingame_key_bindings.update({k: Function(debug_print_key, device='m', key=v) for k, v in ingame_key_bindings_mouse.items()})
+else:
+    map_ingame_key_bindings = {k: Key(f'{v}:down/{min_delay}, {v}:up') for k, v in ingame_key_bindings_kb.items()}
+    map_ingame_key_bindings.update({k: Mouse(f'{v}:down/{min_delay}, {v}:up') for k, v in ingame_key_bindings_mouse.items()})
+
+for (k,v) in map_ingame_key_bindings.items():
+    print(f'{k}:{v}')
+
+
+# spoken phrase to value mappings
+map_colors = {
     "current": "current",
     "gold": "gold",
     "blue": "blue",
     "red": "red",
 }
-tools = {
+map_tools = {
+    "open": "open",
     "c2": "c2",
     "shotgun": "shotgun",
     "shotty": "shotgun",
@@ -43,27 +91,38 @@ tools = {
     "kick down": "kick",
     "kick it": "kick",
     "kick it down": "kick",
-    "open": "open",
 }
-grenades = {
+map_grenades = {
+    "none": "none",
     "bang": "flashbang",
     "flashbang": "flashbang",
     "cs": "gas",
     "gas": "gas",
     "stinger": "stinger",
-    "none": "none"
 }
-launcher_grenades = grenades
+map_launcher_grenades = map_grenades
+map_hold = {
+    "go": "go",
+    "hold": "hold",
+    "on my mark": "hold",
+    "on my order": "hold",
+    "on my command": "hold",
+    "hold for my order": "hold",
+    "hold for my mark": "hold",
+    "hold for my command": "hold",
+}
 
+def cmd_select_team(color):
+    map_ingame_key_bindings[color]
 
 class SelectTeam(CompoundRule):
     spec = "[<color>] team"
-    extras = [Choice("color", ["current", "blue", "red", "gold"])]
+    extras = [Choice("color", map_colors)]
     defaults = {"color": "current"}
 
     def _process_recognition(self, node, extras):
         color = extras["color"]
-        print("%s team" % color)
+        map_ingame_key_bindings[color].execute()
 
 
 class SelectColor(CompoundRule):
@@ -72,19 +131,21 @@ class SelectColor(CompoundRule):
 
     def _process_recognition(self, node, extras):
         color = extras["color"]
-        print("%s team" % color)
+        map_ingame_key_bindings[color].execute()
 
 
 class BreachAndClear(CompoundRule):
-    spec = "[<color>] [team] [<tool>] [the door] [(throw | deploy)] [(<grenade> | fourtymil <launcher> | launch <launcher> | launcher <launcher>)] [and] (breach and clear | clear) [it]"
+    spec = "[<color>] [team] [<hold>] [<tool>] [the door] [([(throw | deploy | use)] <grenade> | [deploy] fourtymil <launcher>)] [and] (breach and clear | clear) [it]"
     extras = [
-        Choice("color", colors),
-        Choice("tool", tools),
-        Choice("grenade", grenades),
-        Choice("launcher", launcher_grenades),
+        Choice("color", map_colors),
+        Choice("hold", map_hold),
+        Choice("tool", map_tools),
+        Choice("grenade", map_grenades),
+        Choice("launcher", map_launcher_grenades),
     ]
     defaults = {
         "color": "current",
+        "hold": "go",
         "tool": "open",
         "grenade": "none",
         "launcher": "none",
@@ -92,9 +153,11 @@ class BreachAndClear(CompoundRule):
 
     def _process_recognition(self, node, extras):
         color = extras["color"]
+        hold = extras["hold"]
         tool = extras["tool"]
         grenade = extras["grenade"]
         launcher = extras["launcher"]
+
         deployed_nade = ""
         if grenade != "none":
             deployed_nade = grenade
@@ -103,8 +166,8 @@ class BreachAndClear(CompoundRule):
         fmt_deployed_nade = ""
         if len(deployed_nade) > 0:
             fmt_deployed_nade = " deploy " + deployed_nade
-        print("{0} team {1} the door{2} breach and clear".format(
-            color, tool, fmt_deployed_nade))
+
+        print(f"{color} team {hold} {tool} the door{fmt_deployed_nade} breach and clear")
 
 
 class YellFreeze(BasicRule):
@@ -112,6 +175,7 @@ class YellFreeze(BasicRule):
         Literal("freeze"),
         Literal("hands"),
         Literal("drop"),
+        Literal("police"),
     ))
 
     def _process_recognition(self, node, extras):
@@ -124,23 +188,19 @@ class FreezeRecob(RecognitionObserver):
 
     def __init__(self):
         RecognitionObserver.__init__(self)
-        self.waiting = False
         self.words = None
         self.frozen = False
 
     def on_begin(self):
-        self.waiting = True
+        self.frozen = False
 
     def on_partial_recognition(self, words, rule):
         self.words = words
         if (not self.frozen) and isinstance(rule, kaldi_active_grammar.KaldiRule) and rule.name == "ReadyOrNot_priority::YellFreeze":
             print("Freeze dirtbag!")
             self.frozen = True
-        # print("rule_type={0}".format(type(rule)))
-        # print("rule={0}".format(rule))
 
     def on_recognition(self, words, results):
-        self.waiting = False
         self.words = words
         # print("words={0}".format(words))
         # print("rule={0}".format(rule))
@@ -148,11 +208,9 @@ class FreezeRecob(RecognitionObserver):
         # print("results={0}".format(results))
 
     def on_failure(self, results):
-        self.waiting = False
         self.words = False
     
     def on_end(self, results):
-        self.waiting = False
         self.words = False
         self.frozen = False
 
