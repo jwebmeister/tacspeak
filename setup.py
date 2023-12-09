445a3987c8a72846d7926114bbd7040ce3a58689
JWebmeister <jwebmeister@gmail.com>
2023-12-09 21:46:32 +1100

    removed g2p_en, updated setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@
 # Licensed under the AGPL-3.0; see LICENSE.txt file.
 #
 
-import os
 import sys
 from os.path import join, basename
-import pkg_resources
+from pkg_resources import get_distribution
 from cx_Freeze import setup, Executable
 
 def collect_dist_info(packages):
@@ -18,50 +17,30 @@ def collect_dist_info(packages):
         packages = [packages]
     dirs = []
     for pkg in packages:
-        distrib = pkg_resources.get_distribution(pkg)
+        distrib = get_distribution(pkg)
         for req in distrib.requires():
             dirs.extend(collect_dist_info(req.key))
         dirs.append((distrib.egg_info, join('Lib', basename(distrib.egg_info))))
     return dirs
 
-def grammar_modules():
-    src_dst_dirs = []
-
-    try:
-        path = os.path.dirname(__file__)
-    except NameError:
-        path = os.getcwd()
-
-    grammar_path = os.path.join(path, os.path.relpath("tacspeak/grammar/"))
-    for filename in os.listdir(grammar_path):
-        file_path = os.path.abspath(os.path.join(grammar_path, filename))
-
-        # Only apply _*.py to files, not directories.
-        is_file = os.path.isfile(file_path)
-        if not is_file:
-            continue
-        if is_file and not (os.path.basename(file_path).startswith("_") and
-                            os.path.splitext(file_path)[1] == ".py"):
-            continue
-        src_dst = (file_path, 
-                   os.path.join(os.path.relpath("tacspeak/grammar/"), os.path.basename(file_path))
-            )
-        src_dst_dirs.append(src_dst)
-
-    return src_dst_dirs
-
 include_files = []
 include_files.extend(collect_dist_info("webrtcvad_wheels"))
-include_files.extend(grammar_modules())
+include_files.append(("tacspeak/grammar/", "tacspeak/grammar/"))
 include_files.append("README.md")
 include_files.append("LICENSE.txt")
 include_files.append(("licenses/pkg_licenses_notices.txt", "licenses/pkg_licenses_notices.txt"))
 include_files.append(("licenses/pkg_licenses_summary.md", "licenses/pkg_licenses_summary.md"))
+include_files.append("kaldi_model/")
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    "packages": ["pkg_resources"],
+    #"packages": ["pkg_resources"],
     "include_files": include_files,
+    # "path": ['C:\\dev\\tacspeak\\.venv', 'C:\\dev\\tacspeak\\.venv\\Lib\\site-packages', 'C:\\dev\\tacspeak\\.venv\\Lib\\site-packages\\win32', 'C:\\dev\\tacspeak\\.venv\\Lib\\site-packages\\win32\\lib', 'C:\\dev\\tacspeak\\.venv\\Lib\\site-packages\\Pythonwin']
+    "excludes": ["tkinter", 
+                 "sqlite3", 
+                 ],
+    "include_msvcr": False,
 }
 
 setup(
