ecbfc45f19ae6421e507c9b9e7becc87f608780a
JWebmeister <jwebmeister@gmail.com>
2024-01-16 18:33:07 +1100

    added dontload_testnotepad.py grammar module

---
+++ tacspeak/grammar/dontload_testnotepad.py
@@ -0,0 +1,38 @@
+#
+# This file is part of Tacspeak.
+# (c) Copyright 2023-2024 by Joshua Webb
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+
+
+from dragonfly import (CompoundRule, AppContext, Grammar)
+from dragonfly.engines.backend_kaldi.dictation import UserDictation as Dictation
+from dragonfly.actions import Text
+
+test_grammar_context = AppContext(executable="notepad")
+test_grammar = Grammar("testnotepad", context=test_grammar_context)
+
+# ----------------------------------------------------------------
+
+class TestNotepadRule(CompoundRule):
+    spec = "<text>"
+    extras = [Dictation("text")]
+
+    def _process_recognition(self, node, extras):
+        # Retrieve the recognized text and format it.
+        text = extras.get("text").format()
+        print(f"text: {text}")
+        Text(f"{text}\n").execute()
+
+test_grammar.add_rule(TestNotepadRule())
+
+# Load the grammar and set it as exclusive, meaning that the engine will
+#  only recognize from this grammar (and any other exclusive grammar).
+test_grammar.load()
+test_grammar.set_exclusiveness(True)
+
+# Unload function which will be called by natlink at unload time.
+def unload():
+    global test_grammar
+    if test_grammar: test_grammar.unload()
+    test_grammar = None
